9732d962cec4b34d9ab2fd6591254c48455c4bab
DESKTOP-NRMDCPQ\Matt <matthew.james.jukes@gmail.com>
2018-03-26 20:44:44 +1300

    Updated Repo

--- Source/SeaOfSands/Private/Weapons/BaseWeapon.cpp
+++ Source/SeaOfSands/Private/Weapons/BaseWeapon.cpp
@@ -11,7 +11,7 @@
 ABaseWeapon::ABaseWeapon()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;	
+	PrimaryActorTick.bCanEverTick = true; 
 
 	//Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 	//Beam = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("Rifle"));
